c8baee3bb6e110cb9f656133cd6ad9ffc7c15326
podaen <Davy.podevijn@gmail.com>
2022-11-05 17:41:19 +0100

    Add header example

---
+++ examples/Header/fnc_wifi.h
@@ -0,0 +1,273 @@
+#pragma once
+/*download the "esp prov" app for android or ios
+to list the networks in the app, be sure that the flash options are:
+Arduino Runs On : Core 1
+Events Run On : Core 0*/
+
+/*# additional Wi-Fi config in sdkconfig (not needed)
+CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM=16
+CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM=64
+CONFIG_ESP32_WIFI_DYNAMIC_TX_BUFFER_NUM=64
+CONFIG_ESP32_WIFI_TX_BA_WIN=32
+CONFIG_ESP32_WIFI_RX_BA_WIN=32
+CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED=y
+CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED=y*/
+
+#include <WiFiProvAP.h>
+#include <WiFi.h>
+
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+
+const char* service_name = "my_device";
+//const char* service_key = "password";
+wifi_prov_security_t security = WIFI_PROV_SECURITY_1;//WIFI_PROV_SECURITY_0
+scheme_handler_t provHandle = WIFI_PROV_SCHEME_HANDLER_NONE;//WIFI_PROV_SCHEME_HANDLER_MAX
+const char* pop = "abcd1234";
+
+int cntNetworks = 0;//WiFiScan
+
+/*ERASE FLASH SETTINGS*/
+void eraseNVS() {
+    esp_err_t ret;
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+    Serial.println("nvs flash erased");
+    ESP_ERROR_CHECK(ret);
+}
+/*EVENTS*/
+void SysProvWiFiEvent(arduino_event_t* sys_event)
+{
+    switch (sys_event->event_id) {
+    case ARDUINO_EVENT_WIFI_READY:
+        Serial.println("WiFi interface ready");
+        break;
+    case ARDUINO_EVENT_WIFI_SCAN_DONE:
+        Serial.println("Completed scan for access points");
+        break;
+    case ARDUINO_EVENT_WIFI_STA_START:
+        Serial.println("WiFi client started");
+        break;
+    case ARDUINO_EVENT_WIFI_STA_STOP:
+        Serial.println("WiFi clients stopped");
+        //auto reconnect is enabled
+        break;
+    case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+        Serial.println("Connected to access point");
+        break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+        Serial.println("\nDisconnected. Connecting to the AP again... ");
+        //auto reconnect is enabled
+        break;
+    case ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE:
+        Serial.println("WiFi sta authmode change");
+        break;
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+        Serial.print("\nConnected IP address : ");
+        Serial.println(IPAddress(sys_event->event_info.got_ip.ip_info.ip.addr));
+        Serial.println("");
+        WiFi.setSleep(false);//full power
+        //Connectivity();
+        PrintFreeGap();
+        beginNTP();
+        connectToMqtt();
+        //connect to clients
+        break;
+    case ARDUINO_EVENT_PROV_START:
+        Serial.println("\nProvisioning started\nGive Credentials of your access point using \" Android app \"");
+        break;
+    case ARDUINO_EVENT_PROV_CRED_RECV: {
+        Serial.println("\nReceived Wi-Fi credentials");
+        Serial.print("\tSSID : ");
+        Serial.println((const char*)sys_event->event_info.prov_cred_recv.ssid);
+        Serial.print("\tPassword : ");
+        Serial.println((char const*)sys_event->event_info.prov_cred_recv.password);
+        break;
+    }
+    case ARDUINO_EVENT_PROV_CRED_FAIL: {
+        Serial.println("\nProvisioning failed!\nPlease reset to factory and retry provisioning\n");
+        if (sys_event->event_info.prov_fail_reason == WIFI_PROV_STA_AUTH_ERROR) {
+            Serial.println("\nWi-Fi AP password incorrect");
+            eraseNVS();
+            ESP.restart();
+        }
+        else {
+            Serial.println("\nWi-Fi AP not found....Add API \" nvs_flash_erase() \" before beginProvision()");
+            eraseNVS();
+            ESP.restart();
+        }
+        break;
+    }
+    case ARDUINO_EVENT_PROV_CRED_SUCCESS:
+        Serial.println("\nProvisioning Successful");
+        break;
+    case ARDUINO_EVENT_PROV_END:
+        Serial.println("\nProvisioning Ends");
+        break;
+    default:
+        break;
+    }
+}
+/*MAIN*/
+void startWifi() {
+    //eraseNVS();//erase credentials and etcetera
+    WiFi.disconnect(true);
+    delay(100);
+    WiFi.onEvent(SysProvWiFiEvent);
+    //scanNetwork();
+    WiFi.setAutoReconnect(true);
+    WiFiProvAP.beginProvisionAP(WIFI_PROV_SCHEME_SOFTAP, WIFI_PROV_SCHEME_HANDLER_NONE, security, pop, service_name);
+    Serial.println("setup done");
+    Serial.println("");
+}
+/*deINI*/
+void deiniWifi() {
+    Serial.println("Deintialize WiFi");
+    WiFi.removeEvent(SysProvWiFiEvent);
+    WiFi.disconnect(true);// delete old config
+    WiFi.mode(WIFI_OFF);//espWiFiStop
+}
+/*NETWORK DETAILS*/
+bool setProtocol() {
+    esp_err_t err;
+    const uint8_t protocol = WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N;
+    err = esp_wifi_set_protocol(WIFI_IF_STA, protocol);
+    if (err != ESP_OK) {
+        //258 = ESP_ERR_INVALID_ARG
+        Serial.println(err);
+        Serial.println("Could not set protocol!");
+        return false;
+    }
+    return true;
+}
+void getProtocol() {
+    esp_err_t err;
+    uint8_t getprotocol;
+    err = esp_wifi_get_protocol(WIFI_IF_STA, &getprotocol);
+    if (err != ESP_OK) {
+        Serial.println("Could not get protocol!");
+        //log_e("Could not get protocol! %d", err);
+    }
+    if (getprotocol & WIFI_PROTOCOL_11N) {
+        Serial.println("WiFi_Protocol_11n");
+    }
+    else
+    {
+        Serial.println("try to set WiFi_Protocol_11n");
+        setProtocol();
+    }
+    if (getprotocol & WIFI_PROTOCOL_11G) {
+        Serial.println("WiFi_Protocol_11g");
+    }
+    if (getprotocol & WIFI_PROTOCOL_11B) {
+        Serial.println("WiFi_Protocol_11b");
+    }
+}
+bool setBandwide() {
+    esp_err_t err;
+    wifi_bandwidth_t wifi_bandwidth = WIFI_BW_HT40;//WIFI_BW_HT20
+    err = esp_wifi_set_bandwidth(WIFI_IF_STA, wifi_bandwidth);//only in ap mode
+    if (err != ESP_OK) {
+        wifi_bandwidth = WIFI_BW_HT20;
+        err = esp_wifi_set_bandwidth(WIFI_IF_STA, wifi_bandwidth);//only in ap mode
+        if (err != ESP_OK) {
+            Serial.println("failed to set bandwide");
+            return false;
+        }
+        else
+        {
+            Serial.println("bandwide set to WIFI_BW_HT20");
+            return true;
+        }
+    }
+    else
+    {
+        Serial.println("bandwide set to WIFI_BW_HT40");
+        return true;
+    }
+}
+void getBandwide() {
+    esp_err_t err;
+    wifi_bandwidth_t wifi_bandwidth;
+    err = esp_wifi_get_bandwidth(WIFI_IF_STA, &wifi_bandwidth);
+    if (err != ESP_OK) {
+        Serial.println("Could not get bandwide!");
+        //log_e("Could not get protocol! %d", err);
+    }
+    if (wifi_bandwidth & WIFI_BW_HT20) {
+        Serial.println("WiFi_Bandwide_WIFI_BW_HT20");
+        setBandwide();
+    }
+    if (wifi_bandwidth & WIFI_BW_HT40) {
+        //WIFI_BW_HT40 is supported only when the interface support 11N
+        Serial.println("WiFi_Bandwide_WIFI_BW_HT40");
+    }
+}
+void getChannel() {
+    //Serial.print("Wifi channel: ");
+//Serial.println(WiFi.channel());
+    esp_err_t err;
+    uint8_t primary;
+    wifi_second_chan_t second;
+    err = esp_wifi_get_channel(&primary, &second);
+    if (err != ESP_OK) {
+        Serial.println("Could not get channel!");
+        //log_e("Could not get protocol! %d", err);
+    }
+    else
+    {
+        Serial.print("primairy channel: ");
+        Serial.println(primary);
+    }
+    if (second & WIFI_SECOND_CHAN_NONE) {//Doesn't print???
+        Serial.println("WiFi_WIFI_SECOND_CHAN_NONE");
+    }
+    if (second & WIFI_SECOND_CHAN_ABOVE) {
+        Serial.println("WiFi_WIFI_SECOND_CHAN_ABOVE");
+    }
+    if (second & WIFI_SECOND_CHAN_BELOW) {
+        Serial.println("WiFi_WIFI_SECOND_CHAN_BELOW");
+    }
+}
+void Connectivity() {
+    esp_err_t err;
+    getProtocol();
+    getBandwide();
+    Serial.println("");
+}
+/*SCAN NETWORK*/
+void printNetwork() {
+    //#define WIFI_SCAN_RUNNING (-1)
+    //#define WIFI_SCAN_FAILED (-2);
+    if (cntNetworks == WIFI_SCAN_FAILED) {
+        Serial.println("Scan Failed!");//returs -2
+        return;
+    }
+    if (!cntNetworks) {
+        Serial.println("no networks found");
+        return;
+    }
+    if (cntNetworks > 0) {
+        Serial.print(cntNetworks);
+        Serial.println(" networks found");
+        for (int i = 0; i < cntNetworks; ++i) {
+            // Print SSID and RSSI for each network found
+            //Serial.print(i + 1);
+            //Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" ch:");
+            Serial.print(WiFi.channel(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            //Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+            delay(10);
+        }
+        Serial.println("");
+    }
+}
+void scanNetwork() {
+    cntNetworks = WiFi.scanNetworks();
+    printNetwork();
+}
